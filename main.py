a4ce9538b48353773e1078c6bdf8ca6336d65008
Henrique Gogó <henriquegogo@gmail.com>
2016-08-26 00:47:22 -0300

    Parsing response and getting info from stock

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import sys
+import sys, requests, json, re
 sys.path.append('lib')
 
 from flask import Flask, request
 app = Flask(__name__, static_url_path='')
+
+from pyquery import PyQuery
 
 @app.route("/")
 def index():
@@ -14,7 +16,19 @@ def update(symbol=None):
 
 @app.route("/statistics/<symbol>")
 def statistics(symbol):
-    return "Hello %s!" % symbol
+    url = "http://www.reuters.com/finance/stocks/financialHighlights?symbol=%s" % symbol
+    response = requests.get(url)
+    dom = PyQuery(response.text)
+    info = {
+        'title': dom('#sectionTitle h1').text(),
+        'symbol': re.search('\((.*)\)', dom('#sectionTitle h1').text()).group(1),
+        'price_to_earnings': dom('td:contains("P/E Ratio")').next().text(),
+        'price_to_book': dom('td:contains("Price to Book")').next().text(),
+        'dividend_yeld': dom('td:contains("Dividend Yield")').next().text(),
+        'current_ratio': dom('td:contains("Current Ratio")').next().text(),
+        'avg_volume': dom('span:contains("Avg. Vol")').next().next().text()
+    }
+    return "%s" % json.dumps(info)
 
 if __name__ == "__main__":
     app.run()
